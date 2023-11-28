a098cc059d2983492a2e622600be09f24eb93853
Clarkson Awomu <clarksonlawson1@gmail.com>
2023-11-28 23:19:10 +0100

    To uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(s):
+    for char in s:
+        uppercase_char = char if not ('a' <= char <= 'z') else chr(ord(char) - ord('a') + ord('A'))
+
+        print("{}".format(uppercase_char), end="")
+
+    print()
