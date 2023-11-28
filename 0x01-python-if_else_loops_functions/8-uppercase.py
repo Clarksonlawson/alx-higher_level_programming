ce2df0d8f8afb04d62689c2f36a451f380a0118a
Clarkson Awomu <clarksonlawson1@gmail.com>
2023-11-28 23:28:02 +0100

    There are only 3 colors, 10 digits, and 7 notes; it's what we do with them that's important

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uppercase(s):
     for char in s:
-        uppercase_char = char if not ('a' <= char <= 'z') else chr(ord(char) - ord('a') + ord('A'))
+        uppercase_char = char if not ('a' <= char <= 'z')  else chr(ord(char) - ord('a') + ord('A'))
 
         print("{}".format(uppercase_char), end="")
 
