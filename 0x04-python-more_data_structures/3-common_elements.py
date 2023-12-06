7d7edddd97e47475220b8def22532e33cf33b7cc
Clarkson Awomu <clarksonlawson1@gmail.com>
2023-12-06 18:34:09 +0100

    whatsup

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    """
+    Returns a set of common elements in two sets.
+
+    :param set_1: The first set.
+    :param set_2: The second set.
+    :return: A set containing common elements between set_1 and set_2.
+    """
+    return set_1 & set_2
