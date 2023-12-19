c549e2190babd1caca8ea4cf8d196aa664d3618b
Clarkson Awomu <clarksonlawson1@gmail.com>
2023-12-19 20:57:30 +0100

    gg

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
-"""Defines a square with size attribute."""
+"""
+Defines a Square class with a size attribute.
+"""
 
 
 class Square:
-    """Represents a square."""
+    """
+    Represents a square.
 
+    Attributes:
+    - size (int): The size of the square.
+    """
     def __init__(self, size=0):
         """Initializes a new instance of the Square class."""
         self.__size = size
