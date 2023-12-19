84cf7930f27212e73d407e7f2a0f9769806c6490
Clarkson Awomu <clarksonlawson1@gmail.com>
2023-12-19 08:41:01 +0100

    working

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,14 @@
+class Square:
+    """Class that defines a square with size validation."""
+    
+    def __init__(self, size=0):
+        """Initialization method with optional size and validation."""
+        self.__size = size
+        self.__validate_size()
+
+    def __validate_size(self):
+        """Private method to validate the size attribute."""
+        if not isinstance(self.__size, int):
+            raise TypeError("size must be an integer")
+        elif self.__size < 0:
+            raise ValueError("size must be >= 0")
