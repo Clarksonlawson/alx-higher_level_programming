dd714a0080cf44c28179d1d541864f2402eeb1af
Clarkson Awomu <clarksonlawson1@gmail.com>
2023-12-19 21:03:27 +0100

    moving

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,15 +1,42 @@
 #!/usr/bin/python3
-class Square:
-    """Class that defines a square with size validation."""
-    
-    def __init__(self, size=0):
-        """Initialization method with optional size and validation."""
-        self.__size = size
-        self.__validate_size()
+"""
+Defines a class Square that represents a square.
+"""
 
-    def __validate_size(self):
-        """Private method to validate the size attribute."""
-        if not isinstance(self.__size, int):
+
+class Square:
+    """
+    This class defines a square with a private instance attribute size.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializes a new Square instance with optional size.
+
+        Parameters:
+        - size (int): The size of the square (default is 0).
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """Gets the size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Sets the size of the square with validation.
+
+        Parameters:
+        - value (int): The size to set.
+
+        Raises:
+        - TypeError: If size is not an integer.
+        - ValueError: If size is less than 0.
+        """
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        elif self.__size < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
+        self.__size = value
