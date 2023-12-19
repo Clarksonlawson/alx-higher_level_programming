e0f10e948fe1413711b9d615f15eea0dbc8c7630
Clarkson Awomu <clarksonlawson1@gmail.com>
2023-12-19 08:16:51 +0100

    contd

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,6 @@
+class Square:
+    """Class that defines a square with a private size attribute."""
+    
+    def __init__(self, size):
+        """Initialization method with a private size attribute."""
+        self.__size = size
