bc98de5bf30a994ac2e02ac91d1d3dce1000deca
Clarkson Awomu <clarksonlawson1@gmail.com>
2024-01-12 17:15:37 +0100

    Random commit message: 742

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -41,3 +41,10 @@ class Base:
         with open(filename, 'w') as file:
             file.write(cls.to_json_string([obj.to_dictionary() for obj in list_objs]))
 
+    @staticmethod
+    def from_json_string(json_string):
+        """Return the list of dictionaries from the JSON string representation."""
+        if json_string is None or len(json_string) == 0:
+            return []
+        return json.loads(json_string)
+
