44db8444e5cf691cdc7d567bf164486f8ee6f4b3
Clarkson Awomu <clarksonlawson1@gmail.com>
2024-02-14 18:46:11 +0100

    Random commit message: 935

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+-- Script to list all records of the table second_table ordered by score in descending order
+
+-- Select the score and name columns from the second_table and order by score in descending order
+SELECT score, name FROM second_table ORDER BY score DESC;
