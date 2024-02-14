aa62c528ed2bb03ad357480cb7dd19fbab19b1c1
Clarkson Awomu <clarksonlawson1@gmail.com>
2024-02-14 18:50:19 +0100

    Random commit message: 409

---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,5 @@
+-- Script to remove all records with a score <= 5 in the table second_table of the database hbtn_0c_0
+
+-- Delete records from second_table where score is less than or equal to 5
+DELETE FROM second_table WHERE score <= 5;
+
