2e2f0e15c184f06e39ed849e54ee60c3a9bb1d56
Clarkson Awomu <clarksonlawson1@gmail.com>
2024-02-14 18:51:01 +0100

    Random commit message: 803

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,4 @@
+-- Script to compute the score average of all records in the table second_table of the database hbtn_0c_0
+
+-- Select the average score from the second_table
+SELECT AVG(score) AS average FROM second_table;
