dca93101bc0c3fe2f356b8b241f752d4a35d08cc
Clarkson Awomu <clarksonlawson1@gmail.com>
2024-02-14 18:36:58 +0100

    Random commit message: 751

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,7 @@
+-- Script to create a table called first_table in the current database
+
+-- Create the table if it does not exist
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
+);
