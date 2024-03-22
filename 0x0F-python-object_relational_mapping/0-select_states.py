8e072c4bd238d0f6e2c9c2bc7cfcbfbb11494016
Clarkson Awomu <clarksonlawson1@gmail.com>
2024-03-22 18:01:47 +0100

    Fixed a bug

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,6 +3,27 @@
 import sys
 import MySQLdb
 
+def select_states(username, password, database):
+    """Select and display all states from the given database."""
+    try:
+        # Connect to MySQL server
+        db = MySQLdb.connect(host="localhost", port=3306, user=username,
+                             passwd=password, db=database, charset="utf8")
+        # Create a cursor object
+        cursor = db.cursor()
+        # Execute SQL query to select all states
+        cursor.execute("SELECT * FROM states ORDER BY id ASC")
+        states = cursor.fetchall()
+        # Display results
+        for state in states:
+            print(state)
+    except MySQLdb.Error as e:
+        print("Error:", e)
+    finally:
+        # Close cursor and database connection
+        cursor.close()
+        db.close()
+
 if __name__ == "__main__":
     # Check if correct number of arguments is provided
     if len(sys.argv) != 4:
@@ -14,32 +35,6 @@ if __name__ == "__main__":
     password = sys.argv[2]
     database = sys.argv[3]
 
-    # Connect to MySQL server
-    try:
-        db = MySQLdb.connect(host="localhost", port=3306, user=username,
-                             passwd=password, db=database, charset="utf8")
-    except MySQLdb.Error as e:
-        print("Error connecting to MySQL database:", e)
-        sys.exit(1)
-
-    # Create a cursor object
-    cursor = db.cursor()
-
-    # Execute SQL query to select all states
-    try:
-        cursor.execute("SELECT * FROM states ORDER BY id ASC")
-        states = cursor.fetchall()
-    except MySQLdb.Error as e:
-        print("Error executing SQL query:", e)
-        cursor.close()
-        db.close()
-        sys.exit(1)
-
-    # Display results
-    for state in states:
-        print(state)
-
-    # Close cursor and database connection
-    cursor.close()
-    db.close()
+    # Call the select_states function
+    select_states(username, password, database)
 
