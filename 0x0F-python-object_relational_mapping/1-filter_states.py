d930b0ba892fac204511732b591d406df9a7d70f
Clarkson Awomu <clarksonlawson1@gmail.com>
2024-03-22 18:08:46 +0100

    Added new feature

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,44 +1,40 @@
 #!/usr/bin/python3
-import MySQLdb
+"""Script that lists all states with a name starting with 'N' from the database hbtn_0e_0_usa."""
 import sys
-def filter_states_by_name_starting_with_N(username, password, db_name):
-        """Retrieve and display states with names starting with 'N'."""
-            try:
-                        # Connect to MySQL server
-                                db = MySQLdb.connect(
-                                                    host="localhost",
-                                                                port=3306,
-                                                                            user=username,
-                                                                                        passwd=password,
-                                                                                                    db=db_name
-                                                                                                            )
-                                        cursor = db.cursor()
-                                                
-                                                        # Execute query to retrieve states with names starting with 'N'
-                                                                cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
-                                                                        
-                                                                                # Fetch all rows
-                                                                                        rows = cursor.fetchall()
-                                                                                                
-                                                                                                        # Display results
-                                                                                                                for row in rows:
-                                                                                                                                print(row)
-                                                                                                                                        
-                                                                                                                                                # Close cursor and database connection
-                                                                                                                                                        cursor.close()
-                                                                                                                                                                db.close()
-                                                                                                                                                                    except MySQLdb.Error as e:
-                                                                                                                                                                                print("MySQL Error:", e)
-                                                                                                                                                                                        sys.exit(1)
+import MySQLdb
 
-                                                                                                                                                                                        if __name__ == "__main__":
-                                                                                                                                                                                                # Check if correct number of arguments provided
-                                                                                                                                                                                                    if len(sys.argv) != 4:
-                                                                                                                                                                                                                print("Usage: {} username password database".format(sys.argv[0]))
-                                                                                                                                                                                                                        sys.exit(1)
-                                                                                                                                                                                                                            
-                                                                                                                                                                                                                                # Get arguments from command line
-                                                                                                                                                                                                                                    username, password, db_name = sys.argv[1:]
-                                                                                                                                                                                                                                        
-                                                                                                                                                                                                                                            # Call the function to filter states by name starting with 'N'
-                                                                                                                                                                                                                                                filter_states_by_name_starting_with_N(username, password, db_name)
+def filter_states(username, password, database):
+    """Filter and display states with names starting with 'N' from the given database."""
+    try:
+        # Connect to MySQL server
+        db = MySQLdb.connect(host="localhost", port=3306, user=username,
+                             passwd=password, db=database, charset="utf8")
+        # Create a cursor object
+        cursor = db.cursor()
+        # Execute SQL query to select states starting with 'N'
+        cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+        states = cursor.fetchall()
+        # Display results
+        for state in states:
+            print(state)
+    except MySQLdb.Error as e:
+        print("Error:", e)
+    finally:
+        # Close cursor and database connection
+        cursor.close()
+        db.close()
+
+if __name__ == "__main__":
+    # Check if correct number of arguments is provided
+    if len(sys.argv) != 4:
+        print("Usage: {} username password database".format(sys.argv[0]))
+        sys.exit(1)
+
+    # Retrieve arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    # Call the filter_states function
+    filter_states(username, password, database)
+
