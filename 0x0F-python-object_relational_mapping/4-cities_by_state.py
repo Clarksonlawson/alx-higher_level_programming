856d5860675db4164bd6ab6fcdf1f2e94d251384
Clarkson Awomu <clarksonlawson1@gmail.com>
2024-03-22 18:28:26 +0100

    Implemented unit tests

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,46 +1,48 @@
 #!/usr/bin/python3
-import MySQLdb
+"""
+Script that lists all cities from the database hbtn_0e_4_usa.
+"""
+
 import sys
+import MySQLdb
 
-def get_cities_by_state(username, password, db_name):
-        """Retrieve and display all cities from the database."""
-            try:
-                        # Connect to MySQL server
-                                db = MySQLdb.connect(
-                                                    host="localhost",
-                                                                port=3306,
-                                                                            user=username,
-                                                                                        passwd=password,
-                                                                                                    db=db_name
-                                                                                                            )
-                                        cursor = db.cursor()
-                                                
-                                                        # Execute query to retrieve cities with corresponding state names
-                                                                cursor.execute("SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.state_id = states.id ORDER BY cities.id")
-                                                                        
-                                                                                # Fetch all rows
-                                                                                        rows = cursor.fetchall()
-                                                                                                
-                                                                                                        # Display results
-                                                                                                                for row in rows:
-                                                                                                                                print(row)
-                                                                                                                                        
-                                                                                                                                                # Close cursor and database connection
-                                                                                                                                                        cursor.close()
-                                                                                                                                                                db.close()
-                                                                                                                                                                    except MySQLdb.Error as e:
-                                                                                                                                                                                print("MySQL Error:", e)
-                                                                                                                                                                                        sys.exit(1)
+def cities_by_state(username, password, database):
+    """Connect to the database and retrieve all cities sorted by state."""
+    try:
+        # Connect to MySQL server
+        db = MySQLdb.connect(host="localhost", port=3306, user=username,
+                             passwd=password, db=database, charset="utf8")
+        # Create a cursor object
+        cursor = db.cursor()
+        # Prepare SQL query
+        query = """SELECT cities.id, cities.name, states.name
+                   FROM cities
+                   JOIN states ON cities.state_id = states.id
+                   ORDER BY cities.id ASC"""
+        # Execute SQL query
+        cursor.execute(query)
+        cities = cursor.fetchall()
+        # Display results
+        for city in cities:
+            print(city)
+    except MySQLdb.Error as e:
+        print("Error:", e)
+    finally:
+        # Close cursor and database connection
+        cursor.close()
+        db.close()
 
-                                                                                                                                                                                        if __name__ == "__main__":
-                                                                                                                                                                                                # Check if correct number of arguments provided                 
-                                                                                                                                                                                                    if len(sys.argv) != 4:
-                                                                                                                                                                                                                print("Usage: {} username password database".format(sys.argv[0]))
-                                                                                                                                                                                                                        sys.exit(1)
-                                                                                                                                                                                                                            
-                                                                                                                                                                                                                                # Get arguments from command line
-                                                                                                                                                                                                                                    username, password, db_name = sys.argv[1:]
-                                                                                                                                                                                                                                        
-                                                                                                                                                                                                                                            # Call the function to get cities by state
-                                                                                                                                                                                                                                                get_cities_by_state(username, password, db_name)
+if __name__ == "__main__":
+    # Check if correct number of arguments is provided
+    if len(sys.argv) != 4:
+        print("Usage: {} username password database".format(sys.argv[0]))
+        sys.exit(1)
+
+    # Retrieve arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    # Call the cities_by_state function
+    cities_by_state(username, password, database)
 
