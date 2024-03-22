1cfe74f3c7c3bbe5d37455dc1a5454131d7397d0
Clarkson Awomu <clarksonlawson1@gmail.com>
2024-03-22 18:11:33 +0100

    Added new feature

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,49 +1,42 @@
 #!/usr/bin/python3
-import MySQLdb
+"""Script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument."""
 import sys
+import MySQLdb
 
-def filter_states_by_name(username, password, db_name, state_name):
-        """Retrieve and display states with names matching the input."""
-            try:
-                        # Connect to MySQL server
-                                db = MySQLdb.connect(
-                                                    host="localhost",
-                                                                port=3306,
-                                                                            user=username,
-                                                                                        passwd=password,
-                                                                                                    db=db_name
-                                                                                                            )
-                                        cursor = db.cursor()
-                                                
-                                                        # Create SQL query with user input
-                                                                sql_query = "SELECT * FROM states WHERE name LIKE %s ORDER BY id"
-                                                                        
-                                                                                # Execute query with user input as parameter
-                                                                                        cursor.execute(sql_query, (state_name,))
-                                                                                                
-                                                                                                        # Fetch all rows
-                                                                                                                rows = cursor.fetchall()
-                                                                                                                        
-                                                                                                                                # Display results
-                                                                                                                                        for row in rows:
-                                                                                                                                                        print(row)
-                                                                                                                                                                
-                                                                                                                                                                        # Close cursor and database connection
-                                                                                                                                                                                cursor.close()
-                                                                                                                                                                                        db.close()
-                                                                                                                                                                                            except MySQLdb.Error as e:
-                                                                                                                                                                                                        print("MySQL Error:", e)
-                                                                                                                                                                                                                sys.exit(1)
+def filter_states(username, password, database, state_name):
+    """Filter and display states where name matches the provided argument."""
+    try:
+        # Connect to MySQL server
+        db = MySQLdb.connect(host="localhost", port=3306, user=username,
+                             passwd=password, db=database, charset="utf8")
+        # Create a cursor object
+        cursor = db.cursor()
+        # Execute SQL query to select states matching the provided argument
+        query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(state_name)
+        cursor.execute(query)
+        states = cursor.fetchall()
+        # Display results
+        for state in states:
+            print(state)
+    except MySQLdb.Error as e:
+        print("Error:", e)
+    finally:
+        # Close cursor and database connection
+        cursor.close()
+        db.close()
 
-                                                                                                                                                                                                                if __name__ == "__main__":
-                                                                                                                                                                                                                        # Check if correct number of arguments provided
-                                                                                                                                                                                                                            if len(sys.argv) != 5:
-                                                                                                                                                                                                                                        print("Usage: {} username password database state_name".format(sys.argv[0]))
-                                                                                                                                                                                                                                                sys.exit(1)
-                                                                                                                                                                                                                                                    
-                                                                                                                                                                                                                                                        # Get arguments from command line
-                                                                                                                                                                                                                                                            username, password, db_name, state_name = sys.argv[1:]
-                                                                                                                                                                                                                                                                
-                                                                                                                                                                                                                                                                    # Call the function to filter states by user input
-                                                                                                                                                                                                                                                                        filter_states_by_name(username, password, db_name, state_name)
+if __name__ == "__main__":
+    # Check if correct number of arguments is provided
+    if len(sys.argv) != 5:
+        print("Usage: {} username password database state_name".format(sys.argv[0]))
+        sys.exit(1)
+
+    # Retrieve arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    # Call the filter_states function
+    filter_states(username, password, database, state_name)
 
