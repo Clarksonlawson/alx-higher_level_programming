a639681175876a45fc07c1deb0ac01d14d9aac3f
Clarkson Awomu <clarksonlawson1@gmail.com>
2024-03-22 17:56:06 +0100

    Added new feature

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,40 +1,37 @@
 #!/usr/bin/python3
-import MySQLdb
+"""
+Script that lists all states from the database hbtn_0e_0_usa
+"""
+
 import sys
+import MySQLdb
 
-def get_states(username, password, db_name):
-        """Retrieve and display all states from the database."""
-            # Connect to MySQL server
-                try:    
-                            db = MySQLdb.connect(
-                                                host="localhost",
-                                                            port=3306,
-                                                                        user=username,
-                                                                                    passwd=password,
-                                                                                                db=db_name
-                                                                                                        )
-                                    cursor = db.cursor()
-                                            # Execute the query to retrieve states
-                                                    cursor.execute("SELECT * FROM states ORDER BY id")
-                                                            # Fetch all rows
-                                                                    rows = cursor.fetchall()
-                                                                            # Display results
-                                                                                    for row in rows:
-                                                                                                    print(row)
-                                                                                                            # Close cursor and database connection
-                                                                                                                    cursor.close()
-                                                                                                                            db.close()
-                                                                                                                                except MySQLdb.Error as e:
-                                                                                                                                            print("MySQL Error:", e)
-                                                                                                                                                    sys.exit(1)
+if __name__ == "__main__":
+    # Connect to MySQL server
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
 
-                                                                                                                                                    if __name__ == "__main__":
-                                                                                                                                                            # Check if correct number of arguments provided
-                                                                                                                                                                if len(sys.argv) != 4:                          
-                                                                                                                                                                            print("Usage: {} username password database".format(sys.argv[0]))
-                                                                                                                                                                                    sys.exit(1)
-                                                                                                                                                                                        # Get arguments from command line
-                                                                                                                                                                                            username, password, db_name = sys.argv[1:]
-                                                                                                                                                                                                # Call the function to get states
-                                                                                                                                                                                                    get_states(username, password, db_name)
+    # Create a cursor object using cursor() method
+    cursor = db.cursor()
+
+    # Execute SQL query to retrieve all states
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+    # Fetch all the rows using fetchall() method
+    rows = cursor.fetchall()
+
+    # Print the results
+    for row in rows:
+        print(row)
+
+    # Close the cursor
+    cursor.close()
+
+    # Close the database connection
+    db.close()
 
