0dce67e301e06cbb566a8aa213a6a6e796d81833
Clarkson Awomu <clarksonlawson1@gmail.com>
2024-05-02 22:45:02 +0100

    h

--- 0x15-javascript-web_jquery/103-script.js
+++ 0x15-javascript-web_jquery/103-script.js
@@ -1,19 +1,18 @@
-$(document).ready(function() {
-    $("#btn_translate").click(function() {
-        fetchTranslation();
-    });
+$(document).ready(function () {
+  $('#btn_translate').click(function () {
+    fetchTranslation();
+  });
 
-    $("#language_code").keypress(function(event) {
-        if (event.which === 13) {
-            fetchTranslation();
-        }
-    });
-
-    function fetchTranslation() {
-        let languageCode = $("#language_code").val();
-        $.get("https://www.fourtonfish.com/hellosalut/hello/?lang=" + languageCode, function(data) {
-            $("#hello").text(data.hello);
-        });
+  $('#language_code').keypress(function (event) {
+    if (event.which === 13) {
+      fetchTranslation();
     }
-});
+  });
 
+  function fetchTranslation () {
+    const languageCode = $('#language_code').val();
+    $.get('https://www.fourtonfish.com/hellosalut/hello/?lang=' + languageCode, function (data) {
+      $('#hello').text(data.hello);
+    });
+  }
+});
